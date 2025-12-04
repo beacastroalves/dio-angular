2a0182cd5e07d29b6a0f8236b481facb1e54b2ae
beacastroalves <beacastro1008@gmail.com>
2025-12-04 12:11:39 +0000

    usando class decorator

--- formação-angular-01/02-typescript-para-angular/src/index.ts
+++ formação-angular-01/02-typescript-para-angular/src/index.ts
@@ -1,2 +1,12 @@
-let dado: string = "Beatriz Castro";
-console.log(dado);
+//Decorators
+function ApiVersion(version: string) {
+  return (target: any) => {
+    Object.assign(target.prototype, { __version: version, __name: "Beatriz" });
+  };
+}
+
+@ApiVersion("1.10")
+class Api {}
+
+const api = new Api();
+console.log(api.__name);
