547b1694c403920fa4e575ee8e184e26f8819557
beacastroalves <beacastro1008@gmail.com>
2025-12-04 20:42:35 +0000

    usando attribute decorators

--- formação-angular-01/02-typescript-para-angular/src/index.ts
+++ formação-angular-01/02-typescript-para-angular/src/index.ts
@@ -1,4 +1,6 @@
 //Decorators
+
+// class decorator
 function ApiVersion(version: string) {
   return (target: any) => {
     Object.assign(target.prototype, { __version: version, __name: "Beatriz" });
@@ -9,4 +11,42 @@ function ApiVersion(version: string) {
 class Api {}
 
 const api = new Api();
+console.log("class decorator:")
 console.log(api.__name);
+console.log("--- --- --- ---")
+
+
+// attribute decorator
+console.log("attribute decorator:")
+
+function minLength(length: number) {
+  return (target: any, key: string) => {
+    let _value = target[key];
+
+    const getter = () => _value;
+    const setter = (value: string) => {
+      if(value.length < length) {
+        throw new Error(`Tamanho menor do que ${length}`);
+      } else {
+        _value = value;
+      }
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+    });
+  }
+}
+
+class Api2 {
+  @minLength(3)
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const api2 = new Api2("usuario");
+console.log(api2.name);
