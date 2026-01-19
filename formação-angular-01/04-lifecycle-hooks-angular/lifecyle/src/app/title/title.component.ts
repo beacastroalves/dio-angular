fcc19f527b1643b8a2510c118446f39d1a4a1ad3
beacastroalves <beacastro1008@gmail.com>
2026-01-19 17:50:56 +0000

    DoCheck e sua ordem de execução

--- formação-angular-01/04-lifecycle-hooks-angular/lifecyle/src/app/title/title.component.ts
+++ formação-angular-01/04-lifecycle-hooks-angular/lifecyle/src/app/title/title.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit, OnChanges {
 
+  @Input() name: string = 'A';
+
+
+  constructor() {
+    console.log(`Constructor ${this.name}`);
+  }
+
+  ngOnChanges(): void {
+    console.log(`OnChanges ${this.name}`);
+  }
+
+  ngOnInit(): void {
+    this.name = this.name + "x"
+    console.log(`OnInit ${this.name}`);
+  }
 }
