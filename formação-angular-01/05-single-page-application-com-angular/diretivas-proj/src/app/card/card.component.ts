2b58a8e0da8329d4aceb450b6592136c1354a445
beacastroalves <beacastro1008@gmail.com>
2026-01-26 21:08:27 +0000

    diretivas - ngFor laço de repetição e conceito de reatividade

--- formação-angular-01/05-single-page-application-com-angular/diretivas-proj/src/app/card/card.component.ts
+++ formação-angular-01/05-single-page-application-com-angular/diretivas-proj/src/app/card/card.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  produtos:string[] = [];
 
+  constructor() {
+    this.produtos = [
+      "mouse",
+      "teclado",
+      "cabo",
+      "fone"
+    ]
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  adicionar() {
+    this.produtos.push("Exemplo de Produto");
+  }
 }
